1b1f8485619eeb91f09d98bfb2aba00179ab2503
José Manuel Nápoles Duarte <jnapoles@uach.mx>
2024-07-01 19:31:17 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,23 @@
 import streamlit as st
-from openai import OpenAI
 import openai
+from openai import OpenAI
 
 # OpenAI API key from Streamlit secrets
 openai.api_key = st.secrets["simple"]
 
+
 def openq(prompt_script):
     client = OpenAI(api_key=st.secrets["simple"])
-    completion = client.chat_completions.create(
-        # model = "gpt-3.5-turbo-0125",
-        # model="gpt-4-turbo-2024-04-09",
-        model="gpt-4o",
-        # model="gpt-3.5-turbo-1106",
-        messages=[
-            {"role": "system", "content": "You are a helpful assistant."},
-            {"role": "user", "content": f"Evaluate the following answers to determine the English proficiency level (Beginner, Intermediate, Advanced):\n{prompt_script}\nProvide the overall proficiency level:"}
-        ],
-        max_tokens=1000
+    completion = client.chat.completions.create(
+       # model = "gpt-3.5-turbo-0125",
+       # model="gpt-4-turbo-2024-04-09",
+      model="gpt-4o",
+      #model="gpt-3.5-turbo-1106",
+      messages=[
+        {"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": f"Evaluate the following answers to determine the English proficiency level (Beginner, Intermediate, Advanced):\n{prompt_script}\nProvide the overall proficiency level:"}
+      ],
+      max_tokens=1000
     )
     return completion.choices[0].message.content
 
